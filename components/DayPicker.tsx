e84de08c4dffbf93acb34e3aaf8b55bf4751cc3f
Jared De Blander <jared0x90@gmail.com>
2023-05-09 04:25:42 -0400

    started date picker

---
+++ components/DayPicker.tsx
@@ -0,0 +1,8 @@
+export function DayPicker() {
+  return (
+    <form action="/" method="get">
+      <label for="day">Pick a day:</label>
+      <input type="date" id="day" name="day" />
+    </form>
+  );
+}
